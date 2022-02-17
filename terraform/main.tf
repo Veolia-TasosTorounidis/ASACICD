ec37a1e6f69cf96ca4b9f6a0880c3b22592c119b
Tasos Torounidis <tasos.torounidis@veolia.com>
2022-02-17 01:14:58 +0000

    more

--- terraform/main.tf
+++ terraform/main.tf
@@ -82,5 +82,5 @@ resource "azurerm_resource_group_template_deployment" "azure_stream_analytics" {
 
   })
 
-  template_content = file("/build/SAJobTemplate.json")
+  template_content = file("./build/SAJobTemplate.json")
 }
